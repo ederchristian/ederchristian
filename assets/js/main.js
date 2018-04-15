ca4cc3d865ba1fed08ba03c53154bd0bb8ebef5f
Eder Christian <ederchristian92@gmail.com>
2018-04-14 22:32:55 -0300

    Remove Facebook script

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,15 +16,6 @@ window.twttr = (function(d, s, id) {
   return t;
 }(document, "script", "twitter-wjs"));
 
-// Facebook
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 // Animate.css
 $('.home').addClass('animated fadeInUp');
 $('.post-header').addClass('animated fadeIn');
