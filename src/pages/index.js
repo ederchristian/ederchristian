187189ca7abcd4ef100b62466673cef6db5b8038
Eder Christian <ederchristian92@gmail.com>
2020-09-20 23:20:53 -0300

    feat: import the Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import Profile from '../components/Profile'
 import SEO from '../components/seo'
 
