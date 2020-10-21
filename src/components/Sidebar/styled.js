ec3091684d68385d2657e893e51d68b0fa529b9f
Eder Christian <ederchristian92@gmail.com>
2020-10-20 22:52:15 -0300

    feat: add the Sidebar component

---
+++ src/components/Sidebar/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const SidebarWrapper = styled.aside`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #28233f;
+  border-right: 1px solid #38444d;
+  height: 100vh;
+  position: fixed;
+  padding: 2rem;
+  text-align: center;
+  width: 24rem;
+`
