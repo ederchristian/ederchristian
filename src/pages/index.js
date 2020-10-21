e2064fd0869164769c8c04e57ed1fec58868729b
Eder Christian <ederchristian92@gmail.com>
2020-10-21 00:34:57 -0300

    feat: remove unused imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
-import Profile from '../components/Profile'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
