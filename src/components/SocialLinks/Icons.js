f6390daa301fdf26bf706ecf01f921630597b691
Eder Christian <ederchristian92@gmail.com>
2020-10-21 00:33:56 -0300

    feat: rename file

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from '@styled-icons/boxicons-logos/Github'
+import { Linkedin } from '@styled-icons/boxicons-logos/Linkedin'
+import { Twitter } from '@styled-icons/boxicons-logos/Twitter'
+import { Instagram } from '@styled-icons/boxicons-logos/Instagram'
+
+const Icons = {
+  Github,
+  Linkedin,
+  Twitter,
+  Instagram
+}
+
+export default Icons
