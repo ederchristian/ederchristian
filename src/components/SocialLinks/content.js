cb01288501cf9925da8cf214d61444b91f0db416
Eder Christian <ederchristian92@gmail.com>
2020-10-20 23:09:21 -0300

    feat: add an array with content for SocialLinks

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,22 @@
+const handle = 'ederchristian92'
+
+const links = [
+  {
+    label: 'GitHub',
+    url: 'https://github.com/ederchristian'
+  },
+  {
+    label: 'LinkedIn',
+    url: `https://linkedin.com/in/${handle}`
+  },
+  {
+    label: 'Twitter',
+    url: `https://twitter.com/${handle}`
+  },
+  {
+    label: 'Instagram',
+    url: `https://instagram.com/${handle}`
+  }
+]
+
+export default links
