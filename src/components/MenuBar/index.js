0ed4ccb17b3c3196547b522f6048fa0a3224ae79
Eder Christian <ederchristian92@gmail.com>
2020-11-02 21:27:39 -0300

    feat: add the MenuBar component

---
+++ src/components/MenuBar/index.js
@@ -0,0 +1,31 @@
+import React from 'React'
+
+import { Home } from '@styled-icons/entypo/Home'
+import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular/SearchAlt2'
+import { UpArrowAlt as Arrow } from '@styled-icons/boxicons-regular/UpArrowAlt'
+import { LightBulb as Light } from '@styled-icons/entypo/LightBulb'
+import { GridHorizontal as Grid } from '@styled-icons/boxicons-regular/GridHorizontal'
+
+import * as S from './styled'
+
+const MenuBar = () => (
+  <S.MenuBarWrapper>
+    <S.MenuBarGroup>
+      <S.MenuBarLink to="/" title="Go back to Home">
+        <S.MenuBarItem><Home /></S.MenuBarItem>
+      </S.MenuBarLink>
+
+      <S.MenuBarLink to="/search" title="Go back to Home">
+        <S.MenuBarItem><Search /></S.MenuBarItem>
+      </S.MenuBarLink>
+    </S.MenuBarGroup>
+
+    <S.MenuBarGroup>
+      <S.MenuBarItem title="Change theme"><Light /></S.MenuBarItem>
+      <S.MenuBarItem title="Change visualization"><Grid /></S.MenuBarItem>
+      <S.MenuBarItem title="Go to top"><Arrow /></S.MenuBarItem>
+    </S.MenuBarGroup>
+  </S.MenuBarWrapper>
+)
+
+export default MenuBar
---
+++ src/components/MenuBar/styled.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+export const MenuBarWrapper = styled.aside`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background: #192734;
+  border-left: 1px solid #38444d;
+  width: 3.75rem;
+  height: 100vh;
+  padding: 0.8rem 0;
+  right: 0;
+`
+
+export const MenuBarGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const MenuBarLink = styled(Link)`
+  display: block;
+`
+
+export const MenuBarItem = styled.span`
+  display: block;
+  color: #8899a6;
+  cursor: pointer;
+  height: 3.75rem;
+  width: 3.75rem;
+  padding: 1.1rem;
+  position: relative;
+
+  &:hover {
+    color: #1fa1f2;
+  }
+`
