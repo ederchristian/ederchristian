a777f61d9909fa2673c9d21674079054f70a003c
Eder Christian <ederchristian92@gmail.com>
2020-12-03 16:50:31 -0300

    feat: add function for Grid and List modes

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -5,17 +5,23 @@ import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular/SearchAlt2'
 import { UpArrowAlt as Arrow } from '@styled-icons/boxicons-regular/UpArrowAlt'
 import { LightBulb as Light } from '@styled-icons/entypo/LightBulb'
 import { GridHorizontal as Grid } from '@styled-icons/boxicons-regular/GridHorizontal'
+import { ListUl as List} from '@styled-icons/boxicons-regular/ListUl'
 
 import * as S from './styled'
 
 const MenuBar = () => {
   const [theme, setTheme] = useState(null)
+  const [display, setDisplay] = useState(null)
 
   const isDarkMode = theme === 'dark'
+  const isListMode = display === 'list'
 
   useEffect(() => {
     setTheme(window.__theme)
+    setDisplay(window.__display)
+
     window.__onThemeChange = () => setTheme(window.__theme)
+    window.__onDisplayChange = () => setDisplay(window.__display)
   }, [])
 
   return (
@@ -36,7 +42,14 @@ const MenuBar = () => {
         >
           <Light />
         </S.MenuBarItem>
-        <S.MenuBarItem title="Change visualization"><Grid /></S.MenuBarItem>
+        <S.MenuBarItem
+          title="Change visualization"
+          onClick={() => {
+            window.__setPreferredDisplay(isListMode ? 'grid' : 'list')
+          }}
+        >
+          {isListMode ? <Grid /> : <List />}
+        </S.MenuBarItem>
         <S.MenuBarItem title="Go to top"><Arrow /></S.MenuBarItem>
       </S.MenuBarGroup>
     </S.MenuBarWrapper>
