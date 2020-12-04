7a252456187e733d5a88a9e789b60e30e8167e75
Eder Christian <ederchristian92@gmail.com>
2020-12-03 22:17:17 -0300

    refactor: remove unused import

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
 import { Home } from '@styled-icons/entypo/Home'
-import { SearchAlt2 as Search } from '@styled-icons/boxicons-regular/SearchAlt2'
 import { UpArrowAlt as Arrow } from '@styled-icons/boxicons-regular/UpArrowAlt'
 import { LightBulb as Light } from '@styled-icons/entypo/LightBulb'
 import { GridHorizontal as Grid } from '@styled-icons/boxicons-regular/GridHorizontal'
