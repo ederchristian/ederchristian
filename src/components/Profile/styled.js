813822f2ad1c2c752cbd79906ef6f0d5dda9d13c
Eder Christian <ederchristian92@gmail.com>
2021-01-06 22:44:21 -0300

    styled: add a new layout for desktop

--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -1,11 +1,20 @@
-import styled from 'styled-components'
-import media from 'styled-media-query'
-import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import styled from "styled-components"
+import media from "styled-media-query"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 export const ProfileWrapper = styled.section`
-  color: var(--texts);
-  display: flex;
+  display: ${props => (props.isMobileHeader ? 'none' : 'flex')};
   flex-direction: column;
+  color: var(--texts);
+
+  ${media.lessThan('large')`
+    display: ${props => (props.isMobileHeader ? 'flex' : 'none')};
+    align-items: flex-start;
+    background: var(--mediumBackground);
+    border-bottom: 1px solid var(--borders);
+    padding: 1rem;
+    width: 100vw;
+  `}
 `
 
 export const ProfileLink = styled(AniLink)`
