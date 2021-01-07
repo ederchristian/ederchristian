7cb40bbbfead17cf7af4924d4f385c0491aa5166
Eder Christian <ederchristian92@gmail.com>
2021-01-06 22:41:28 -0300

    feat: add trackers

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -5,15 +5,24 @@ import { UpArrowAlt as Arrow } from '@styled-icons/boxicons-regular/UpArrowAlt'
 import { LightBulb as Light } from '@styled-icons/entypo/LightBulb'
 import { GridHorizontal as Grid } from '@styled-icons/boxicons-regular/GridHorizontal'
 import { ListUl as List} from '@styled-icons/boxicons-regular/ListUl'
+import { Menu } from '@styled-icons/boxicons-regular/Menu'
+
+import getThemeColor from '../../utils/getThemeColor'
 
 import * as S from './styled'
 
-const MenuBar = () => {
+import * as GA from './trackers'
+
+const MenuBar = ({ setIsMenuOpen, isMenuOpen }) => {
   const [theme, setTheme] = useState(null)
   const [display, setDisplay] = useState(null)
 
   const isDarkMode = theme === 'dark'
   const isListMode = display === 'list'
+
+  if (theme !== null) {
+    GA.themeTracker(theme)
+  }
 
   useEffect(() => {
     setTheme(window.__theme)
@@ -23,6 +32,11 @@ const MenuBar = () => {
     window.__onDisplayChange = () => setDisplay(window.__display)
   }, [])
 
+  const openMenu = () => {
+    GA.menuTracker()
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <S.MenuBarWrapper>
       <S.MenuBarGroup>
@@ -31,20 +45,40 @@ const MenuBar = () => {
           title="Go back to Home"
           cover
           direction="right"
-          bg="#0e1218"
+          bg={getThemeColor()}
           duration={0.5}
+          activeClassName="active"
         >
-          <S.MenuBarItem><Home /></S.MenuBarItem>
+          <S.MenuBarItem>
+            <Home />
+          </S.MenuBarItem>
         </S.MenuBarLink>
       </S.MenuBarGroup>
+
+      <S.MenuBarGroupMobile>
+        <S.MenuBarGroup>
+          <S.MenuBarItem title="Open Menu" onClick={openMenu}>
+            <Menu />
+          </S.MenuBarItem>
+        </S.MenuBarGroup>
+      </S.MenuBarGroupMobile>
 
       <S.MenuBarGroup>
         <S.MenuBarItem
           title="Change theme"
           onClick={() => {
             window.__setPreferredTheme(isDarkMode ? 'light' : 'dark')
+
+            if (window.DISQUS !== undefined) {
+              window.setTimeout(() => {
+                window.DISQUS.reset({
+                  reload: true
+                })
+              }, 300)
+            }
           }}
           className={theme}
+          isDarkMode={isDarkMode}
         >
           <Light />
         </S.MenuBarItem>
