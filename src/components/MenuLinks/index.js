d78346047dfff16c0ccae05a6d25c185beed544e
Eder Christian <ederchristian92@gmail.com>
2021-01-06 22:42:33 -0300

    feat: add new props

--- src/components/MenuLinks/index.js
+++ src/components/MenuLinks/index.js
@@ -1,28 +1,44 @@
-import React from 'react'
+import React from "react"
+import ReactGA from "react-ga"
 
-import links from './content'
+import links from "./content"
 
-import * as S from './styled'
+import getThemeColor from '../../utils/getThemeColor'
 
-const MenuLinks = () => (
-  <S.MenuLinksWrapper>
-    <S.MenuLinksList>
-      {links.map((link, i) => (
-        <S.MenuLinksItem key={i}>
-          <S.MenuLinksLink
-            to={link.url}
-            cover
-            direction="left"
-            bg="#0e1218"
-            duration={0.5}
-            activeClassName="active"
-          >
-            {link.label}
-          </S.MenuLinksLink>
-        </S.MenuLinksItem>
-      ))}
-    </S.MenuLinksList>
-  </S.MenuLinksWrapper>
-)
+import * as S from "./styled"
+
+const MenuLinks = ({ setIsMenuOpen, isMenuOpen }) => {
+  const menuLinkClickTrack = link => {
+    setIsMenuOpen(!isMenuOpen)
+
+    ReactGA.event({
+      category: 'menu link',
+      action: 'click',
+      label: `Menu Link - ${link}`
+    })
+  }
+
+  return (
+    <S.MenuLinksWrapper>
+      <S.MenuLinksList>
+        {links.map((link, i) => (
+          <S.MenuLinksItem key={i}>
+            <S.MenuLinksLink
+              to={link.url}
+              cover
+              direction="left"
+              bg={getThemeColor()}
+              duration={0.5}
+              onClick={() => menuLinkClickTrack(link.label)}
+              activeClassName="active"
+            >
+              {link.label}
+            </S.MenuLinksLink>
+          </S.MenuLinksItem>
+        ))}
+      </S.MenuLinksList>
+    </S.MenuLinksWrapper>
+  )
+}
 
 export default MenuLinks
