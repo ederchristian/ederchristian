ae630c28cb5f7e0f9eb8673d501e0ac43600f69a
Eder Christian <ederchristian92@gmail.com>
2021-01-06 22:43:00 -0300

    styled: add a new layout for desktop

--- src/components/MenuLinks/styled.js
+++ src/components/MenuLinks/styled.js
@@ -1,23 +1,41 @@
-import styled from 'styled-components'
-import media from 'styled-media-query'
-import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import styled from "styled-components"
+import media from "styled-media-query"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 export const MenuLinksWrapper = styled.nav`
   ${media.lessThan("large")`
-    display: none;
+    margin: auto;
   `}
 `
 
 export const MenuLinksList = styled.ul`
   font-size: 1.2rem;
   font-weight: 300;
+
+  ${media.lessThan('large')`
+    font-size: 1.8rem;
+  `}
 `
 
 export const MenuLinksItem = styled.li`
   padding: 0.5rem 0;
 
+  ${media.lessThan('large')`
+    padding: 1rem 0;
+  `}
+
   .active {
     color: var(--highlight);
+  }
+
+  a {
+    color: var(--texts);
+    text-decoration: none;
+    transition: all 0.5 ease;
+
+    &:hover {
+      color: var(--highlight);
+    }
   }
 `
 
