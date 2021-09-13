45d1758872f87f9188ef356af7d1e6acb5bad9ab
Eder Christian <ederchristian92@gmail.com>
2021-09-13 00:27:14 -0300

    refactor: update Instagram URL

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -1,22 +1,22 @@
-const handle = 'ederchristian92'
+const handle = "ederchristian92"
 
 const links = [
   {
-    label: 'Github',
-    url: 'https://github.com/ederchristian'
+    label: "Github",
+    url: "https://github.com/ederchristian",
   },
   {
-    label: 'Linkedin',
-    url: `https://linkedin.com/in/${handle}`
+    label: "Linkedin",
+    url: `https://linkedin.com/in/${handle}`,
   },
   {
-    label: 'Twitter',
-    url: `https://twitter.com/${handle}`
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
   },
   {
-    label: 'Instagram',
-    url: `https://instagram.com/${handle}`
-  }
+    label: "Instagram",
+    url: `https://instagram.com/ederchristian`,
+  },
 ]
 
 export default links
