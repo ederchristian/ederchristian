e040dc46d61bd37aafd9c1c4f8a5a6c7bbbc8bfa
Eder Christian <ederchristian92@gmail.com>
2021-09-13 23:09:46 -0300

    style: add a new layout for icons on desktop

--- src/components/SocialLinks/styled.js
+++ src/components/SocialLinks/styled.js
@@ -2,10 +2,7 @@ import styled from "styled-components"
 import media from "styled-media-query"
 
 export const SocialLinksWrapper = styled.nav`
-  display: none;
-
   ${media.lessThan("large")`
-    display: block;
     order: 2;
     margin: 0 0 1rem;
     width: 100%;
@@ -17,9 +14,18 @@ export const SocialLinksList = styled.ul`
   align-items: center;
   justify-content: space-around;
   list-style: none;
+
+  ${media.greaterThan("large")`
+    border-right: 1px solid var(--borders);
+    padding-right: 3.2rem;
+  `}
 `
 
-export const SocialLinksItem = styled.li``
+export const SocialLinksItem = styled.li`
+  ${media.greaterThan("large")`
+    margin-left: 1.6rem;
+  `}
+`
 
 export const SocialLinksLink = styled.a`
   color: var(--texts);
@@ -35,4 +41,9 @@ export const IconWrapper = styled.div`
   fill: #fff;
   width: 32px;
   height: 32px;
+
+  ${media.greaterThan("large")`
+    width: 1.6rem;
+    height: 1.6rem;
+  `}
 `
