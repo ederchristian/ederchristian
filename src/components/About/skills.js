e48d215b2de3dad82e2e34f2d5f57239ffc02fc0
Eder Christian <ederchristian92@gmail.com>
2021-09-14 22:48:46 -0300

    feat: add new arrays for the About page

---
+++ src/components/About/skills.js
@@ -0,0 +1,72 @@
+export const Skills = [
+  {
+    id: 1,
+    item: "HTML",
+  },
+  {
+    id: 2,
+    item: "CSS",
+  },
+  {
+    id: 3,
+    item: "JavaScript",
+  },
+  {
+    id: 4,
+    item: "Bootstrap 3 and 4",
+  },
+  {
+    id: 5,
+    item: "React",
+  },
+  {
+    id: 6,
+    item: "SVG",
+  },
+  {
+    id: 7,
+    item: "Wordpress",
+  },
+  {
+    id: 8,
+    item: "BEM",
+  },
+  {
+    id: 9,
+    item: "TDD",
+  },
+  {
+    id: 10,
+    item: "Docker",
+  },
+  {
+    id: 11,
+    item: "Git",
+  },
+  {
+    id: 12,
+    item: "Unix Commands",
+  },
+  {
+    id: 13,
+    item: "macOS",
+  },
+  {
+    id: 14,
+    item: "Linux",
+  },
+  {
+    id: 15,
+    item: "Sketch",
+  },
+  {
+    id: 16,
+    item: "Hubspot",
+  },
+  {
+    id: 17,
+    item: "Scrum",
+  },
+]
+
+export default Skills
---
+++ src/components/About/studiesFuture.js
@@ -0,0 +1,12 @@
+export const StudiesFuture = [
+  {
+    id: 1,
+    item: "Vue.js",
+  },
+  {
+    id: 2,
+    item: "Swift",
+  },
+]
+
+export default StudiesFuture
