5669ead872b8adaf08997cfc4251aeeccd541644
Eder Christian <ederchristian92@gmail.com>
2024-05-15 11:06:57 -0300

    Update dependecies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import icon from "astro-icon";
 
-import react from "@astrojs/react";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   site: 'https://ederchrisitan.com',
   integrations: [mdx(), sitemap(), icon(), react()],
   i18n: {
