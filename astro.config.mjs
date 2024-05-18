47a0cab23fe3ef64e7d18a026ea83acb7ac0b89f
Eder Christian <ederchristian92@gmail.com>
2024-05-18 12:28:25 -0300

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
+import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
-import icon from "astro-icon";
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     imageService: 'cloudflare'
- }),
-  site: 'https://ederchrisitan.com',
+  }),
+  site: 'https://ederchristian.com',
   integrations: [mdx(), sitemap(), icon(), react()],
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "pt"],
+    defaultLocale: 'en',
+    locales: ['en', 'pt'],
     routing: {
       prefixDefaultLocale: false
     }
@@ -25,5 +24,16 @@ export default defineConfig({
     minify: true,
     sourcemap: true,
     target: 'esnext'
-  }
+  },
+  vite: {
+    build: {
+      minify: 'terser',
+      sourcemap: true,
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
+      },
+    },
+  },
 });
