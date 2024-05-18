a145488fa4bf808f2618320527de6a5cc41e78f0
Eder Christian <ederchristian92@gmail.com>
2024-05-18 19:57:16 -0300

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,14 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
+import vercelServerless from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    imageService: {
-      service: 'cloudflare'
-    }
-  }),
-  image: {
-    service: {
-       entrypoint: 'astro/assets/services/sharp',
-       config: {
-         limitInputPixels: false,
-      },
-      options: {
-        formats: ['jpeg', 'png', 'webp']
-      }
-     },
-  },
+  adapter: vercelServerless(),
   site: 'https://ederchristian.com',
   integrations: [mdx(), sitemap(), icon(), react()],
   i18n: {
@@ -32,38 +17,5 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
     }
-  },
-  build: {
-    minify: true,
-    sourcemap: true,
-    target: 'esnext'
-  },
-  vite: {
-    build: {
-      minify: true,
-      sourcemap: true,
-      target: 'esnext',
-      minifier: 'terser',
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          }
-        }
-      }
-    },
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    },
-    optimizeDeps: {
-      include: ['astro:assets'],
-    },
-    experimental: {
-      minify: true,
-    },
   },
 });
