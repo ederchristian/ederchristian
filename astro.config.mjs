c4044e1ff504a0cd1dae65d1b4060a954f159d19
Eder Christian <ederchristian92@gmail.com>
2024-05-19 20:06:00 -0300

    Add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,18 @@ import react from '@astrojs/react';
 import vercelServerless from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown'
 import icon from 'astro-icon';
 
 export default defineConfig({
   output: 'server',
   adapter: vercelServerless(),
   site: 'https://ederchristian.com',
-  integrations: [mdx(), sitemap(), icon(), react()],
+  integrations: [mdx(), sitemap(), icon(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pt'],
